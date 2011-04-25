314ce037ba6a917623dc895c36a8eb7492c49ae7
Darrell Hamilton <darrell.noice@gmail.com>
2011-04-25 00:49:23 -0700

    Array utility class to ease access of array elements

---
+++ Scratch/Source/Utils/ArrayUtils.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Scratch\Utils;
+
+class ArrayUtils {
+	static function &get(&$arr,$key,$default) {
+		$ret = null;
+		if(array_key_exists($key,$arr)) {
+			$ret = $arr[$key];
+		} else if(func_num_args() > 2) {
+			$ret = $default;
+		} else {
+			throw new Exception("The key '{$key}' does not exist in the given array");
+		}
+
+		return $ret;
+	}
+}
