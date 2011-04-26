cdff859ebff13e449634b542cf80eac4e79004ca
Darrell Hamilton <darrell.noice@gmail.com>
2011-04-25 22:31:57 -0700

    added default value for default to supress warnings.  fixed return by reference

--- Scratch/Source/Utils/ArrayUtils.php
+++ Scratch/Source/Utils/ArrayUtils.php
@@ -3,14 +3,14 @@
 namespace Scratch\Utils;
 
 class ArrayUtils {
-	static function &get(&$arr,$key,$default) {
+	static function &get(&$arr,$key,$default=null) {
 		$ret = null;
 		if(array_key_exists($key,$arr)) {
-			$ret = $arr[$key];
+			$ret = &$arr[$key];
 		} else if(func_num_args() > 2) {
 			$ret = $default;
 		} else {
-			throw new Exception("The key '{$key}' does not exist in the given array");
+			throw new \Exception("The key '{$key}' does not exist in the given array");
 		}
 
 		return $ret;
